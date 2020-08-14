18b0f6ab4059893546875238b5361b88287b1788
wandl <hiwepy@163.com>
2020-08-14 12:18:56 +0800

    xg

--- src/main/java/com/aliyun/openservices/spring/boot/AbstractMessageOrderListener.java
+++ src/main/java/com/aliyun/openservices/spring/boot/AbstractMessageOrderListener.java
@@ -1,27 +1,37 @@
 package com.aliyun.openservices.spring.boot;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.aliyun.openservices.ons.api.Message;
 import com.aliyun.openservices.ons.api.order.ConsumeOrderContext;
 import com.aliyun.openservices.ons.api.order.MessageOrderListener;
 import com.aliyun.openservices.ons.api.order.OrderAction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class AbstractMessageOrderListener implements MessageOrderListener {
 
     private final Logger log = LoggerFactory.getLogger(AbstractMessageOrderListener.class);
-
+	private final Function<Message, Integer> function;
+	private final BiConsumer<Integer, Message> consumer;
+	
+	public AbstractMessageOrderListener(Function<Message, Integer> function, BiConsumer<Integer, Message> consumer) {
+		this.function = function;
+		this.consumer = consumer;
+	}
+	
     @Override
     public OrderAction consume(Message message, ConsumeOrderContext context) {
         log.info("MessageOrderListener start msgKey:{},topic:{},body：{}", message.getKey(), message.getTopic(), new String(message.getBody()));
-        int count = countMqFlowByMqFlowId(message.getKey());
+        int count = function.apply(message);
         if (count != 0) {
             log.warn("MessageOrderListener repeat consume  msgKey:{},topic:{},body：{}", message.getKey(), message.getTopic(), new String(message.getBody()));
             return OrderAction.Success;
         }
         try {
-            orderConsume(message);
-            saveMqFlow(message.getKey(), new String(message.getBody()));
+        	consumer.accept(count, message);
             return OrderAction.Success;
         } catch (Exception e) {
             log.error("consume error topic:{},msgKey:{}", message.getTopic(), message.getKey(), e);
@@ -29,18 +39,5 @@ public abstract class AbstractMessageOrderListener implements MessageOrderListen
             return OrderAction.Suspend;
         }
     }
-
-
-    /**
-     * @param message :
-     * @Author: 顺序消费
-     * @Description:子类实现
-     * @Date: 2020/7/15
-     */
-    public abstract void orderConsume(Message message) throws Exception;
-
-    public abstract Integer countMqFlowByMqFlowId(String msgKey);
-
-    public abstract void saveMqFlow(String msgKey, String msgBody);
 
 }
