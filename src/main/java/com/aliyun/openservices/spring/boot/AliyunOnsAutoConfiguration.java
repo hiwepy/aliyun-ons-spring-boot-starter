17c52b079eee4419a742ecb104fe17f17162161d
wandl <hiwepy@163.com>
2021-10-22 13:44:36 +0800

    去除默认的消费者，导致异常

--- src/main/java/com/aliyun/openservices/spring/boot/AliyunOnsAutoConfiguration.java
+++ src/main/java/com/aliyun/openservices/spring/boot/AliyunOnsAutoConfiguration.java
@@ -1,22 +1,15 @@
 package com.aliyun.openservices.spring.boot;
 
-import java.util.Map;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.aliyun.openservices.ons.api.MessageListener;
 import com.aliyun.openservices.ons.api.ONSFactory;
 import com.aliyun.openservices.ons.api.Producer;
-import com.aliyun.openservices.ons.api.bean.ConsumerBean;
-import com.aliyun.openservices.ons.api.bean.OrderConsumerBean;
 import com.aliyun.openservices.ons.api.bean.OrderProducerBean;
 import com.aliyun.openservices.ons.api.bean.ProducerBean;
-import com.aliyun.openservices.ons.api.bean.Subscription;
-import com.aliyun.openservices.ons.api.order.MessageOrderListener;
 import com.aliyun.openservices.ons.api.order.OrderProducer;
 
 @Configuration
@@ -40,28 +33,6 @@ public class AliyunOnsAutoConfiguration {
         producerBean.setProperties(onsMqProperties.toProperties(onsProperties));
         producerBean.start();
         return producerBean;
-    }
-    
-    @Bean(destroyMethod = "shutdown")
-    @ConditionalOnMissingBean
-    public ConsumerBean consumerBean(AliyunProperties onsProperties, AliyunOnsMqProperties onsMqProperties, AliyunOnsMqTemplate aliyunOnsMqTemplate) {
-        ConsumerBean consumerBean = new ConsumerBean();
-        consumerBean.setProperties(onsMqProperties.toConsumerProperties(onsProperties));
-        Map<Subscription, MessageListener> subscriptionTable = aliyunOnsMqTemplate.getSubscriptionTable();
-        consumerBean.setSubscriptionTable(subscriptionTable);
-        consumerBean.start();
-        return consumerBean;
-    }
-    
-    @Bean(destroyMethod = "shutdown")
-    @ConditionalOnMissingBean
-    public OrderConsumerBean orderConsumerBean(AliyunProperties onsProperties, AliyunOnsMqProperties onsMqProperties, AliyunOnsMqTemplate aliyunOnsMqTemplate) {
-        OrderConsumerBean consumerBean = new OrderConsumerBean();
-        consumerBean.setProperties(onsMqProperties.toConsumerProperties(onsProperties));
-        Map<Subscription, MessageOrderListener> subscriptionTable = aliyunOnsMqTemplate.getOrderSubscriptionTable();
-        consumerBean.setSubscriptionTable(subscriptionTable);
-        consumerBean.start();
-        return consumerBean;
     }
     
 	@Bean
